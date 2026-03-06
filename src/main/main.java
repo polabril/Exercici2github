40309b0a94cf7969a92e2a5652e88ac3be71d0e5
dam1 <dam1@bceca02e011d>
2026-03-06 20:09:21 +0100

    3a modificació Git

--- src/main/main.java
+++ src/main/main.java
@@ -3,7 +3,9 @@ package main;
 public class main {
 
 	public static void main(String[] args) {
-	System.out.println("2a modificació Github");
+	System.out.println("1a modificació Git");
+	System.out.println("2a Modificació Github");	
+	System.out.println("3a modificació Git");
 
 
 	}
